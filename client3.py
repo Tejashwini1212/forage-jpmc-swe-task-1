38e04d948545916315c0ddc0d4a162c6272f1516
Tejashwini1212 <121215868+Tejashwini1212@users.noreply.github.com>
2024-07-04 20:41:09 +0530

    Fixed client datafeed script

--- client3.py
+++ client3.py
@@ -20,40 +20,35 @@
 
 import json
 import random
+import sys
 import urllib.request
 
-# Server API URLs
-QUERY = "http://localhost:8080/query?id={}"
+# Given an array of stock data, find the ratio of the two prices
+def getRatio(price_a, price_b):
+    """ Get ratio of price_a to price_b. """
+    if price_b == 0:
+        return
+    return price_a / price_b
 
-# 500 server request
-N = 500
-
-
+# Fetch and format data from the API
 def getDataPoint(quote):
-    """ Produce all the needed values to generate a datapoint """
-    """ ------------- Update this function ------------- """
+    """ Produce all of the needed values to generate a datapoint """
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price) / 2
     return stock, bid_price, ask_price, price
 
-
-def getRatio(price_a, price_b):
-    """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return 1
-
-
-# Main
+# Main function
 if __name__ == "__main__":
-    # Query the price once every N seconds.
-    for _ in iter(range(N)):
-        quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
+    # Query the API once every N seconds and update stock data
+    for _ in range(500):
+        quotes = json.loads(urllib.request.urlopen("http://localhost:8080/query?datatype=json").read())
 
-        """ ----------- Update to get the ratio --------------- """
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
-            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+            print(f"Quoted {stock} at (bid:{bid_price}, ask:{ask_price}, price:{price})")
 
-        print("Ratio %s" % getRatio(price, price))
+        price_a = getDataPoint(quotes[0])[3]
+        price_b = getDataPoint(quotes[1])[3]
+        print(f"Ratio {getRatio(price_a, price_b)}")
